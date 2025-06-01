76c8d49a8afd2b4ed2b58d78cce0277ad2cb9ce5
jyasuu <s1002605@pu.edu.tw>
2025-06-01 03:36:33 +0000

    simple service

--- blazor-demo/Model/Data/PizzaService.cs
+++ blazor-demo/Model/Data/PizzaService.cs
@@ -11,18 +11,13 @@ using Blazored.LocalStorage;
 public class PizzaService
 {
     
-    private readonly ILocalStorageService _localStorage;
-    private readonly ILogger<PizzaService> _logger;
-    private const string PizzasKey = "Pizzas";
 
-    public PizzaService(ILocalStorageService localStorage, ILogger<PizzaService> logger)
+    public PizzaService()
     {
-        _localStorage = localStorage;
-        _logger = logger;
 
     }
 
-    public async Task<List<Pizza>> GetPizzasAsync()
+    public List<Pizza> GetPizzas()
     {
         {
             var pizzas = new List<Pizza>();
@@ -34,24 +29,9 @@ public class PizzaService
             pizza.Vegetarian = false;
             pizza.Vegan = false;
             pizzas.Add(pizza);
-
-            _localStorage.SetItemAsync(PizzasKey, pizzas);
-        }
-        
-        {
-            
-            var pizzas = await _localStorage.GetItemAsync<List<Pizza>>(PizzasKey) ?? new List<Pizza>();
-            var pizza = new Pizza();
-            pizza.PizzaId = 1;
-            pizza.Name = "test";
-            pizza.Description = "test desc";
-            pizza.Price = 1;
-            pizza.Vegetarian = false;
-            pizza.Vegan = false;
-            
-            pizzas.Add(pizza);
             return pizzas;
         }
+        
     }
 
 }
